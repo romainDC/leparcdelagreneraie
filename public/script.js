69b955bbe23b27296a6eb7a8020a0471f5b6b523
Romain De Carvalho <romain.decarvalho@smile.fr>
2025-06-17 17:03:31 +0200

    Masquage contacts

--- public/script.js
+++ public/script.js
@@ -1,3 +1,8 @@
-/**
- * 
- */
+let cdb = document.getElementById("contactDetailButton");
+let contact = document.getElementById("contact");
+let mail = document.getElementById("mail2");
+
+cdb.onclick = () => {
+    if (mail.innerHTML === "")
+        contact.style.display = "initial";
+}
